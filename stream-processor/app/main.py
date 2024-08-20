582904aed7e40e8186438ee6a9114a933545ac27
홍원기 <mankiplayer@gmail.com>
2024-08-20 18:14:53 +0900

    improve event stream handling

--- stream-processor/app/main.py
+++ stream-processor/app/main.py
@@ -1,7 +1,8 @@
-from dataclasses import dataclass
-from operator import attrgetter
+from dataclasses import dataclass, field
+from operator import attrgetter, is_not
+from functools import partial
 import json
-from typing import Literal
+from typing import Literal, Self
 
 from bytewax.dataflow import Dataflow
 from bytewax import operators as op
@@ -9,152 +10,53 @@ from bytewax.testing import TestingSource
 
 from app.model import (
     Campaign,
-    UserEventData,
     CampaignChangeData,
-    CampaignEventType,
+    UserEventData,
 )
 from app.sink import CampaignServiceSink
 from pb.campaign_service_pb2 import UserEventMessage
 
+CampaignEventType = Literal["trigger_event", "exception_event"]
+
 flow = Dataflow("crm")
 
-# 1. Flat map by create/update/delete events
-# 2. Key by events
-# 3. Stateful by event
-#   -> for event X, trigger campaigns {a,b} and cancel campaign {c}
 campaign_cdc_data = [
     CampaignChangeData(
         op="c",
         before=None,
-        after=Campaign(id=1, trigger_event="event_A", exception_event=None),
+        after=Campaign(id="doc1", status="active", trigger_event="event_A", exception_event=None),
     ),
     CampaignChangeData(
         op="c",
         before=None,
-        after=Campaign(id=2, trigger_event="event_B", exception_event=None),
+        after=Campaign(id="doc2", status="active", trigger_event="event_B", exception_event=None),
     ),
     CampaignChangeData(
         op="c",
         before=None,
-        after=Campaign(id=3, trigger_event="event_A", exception_event="event_B"),
+        after=Campaign(id="doc3", status="active", trigger_event="event_A", exception_event="event_B"),
     ),
     CampaignChangeData(
         op="u",
-        before=Campaign(id=1, trigger_event="event_A", exception_event=None),
-        after=Campaign(id=1, trigger_event="event_A", exception_event="event_C"),
+        before=Campaign(id="doc1", status="active", trigger_event="event_A", exception_event=None),
+        after=Campaign(id="doc1", status="active", trigger_event="event_A", exception_event="event_C"),
     ),
     CampaignChangeData(
         op="c",
         before=None,
-        after=Campaign(id=4, trigger_event="event_C", exception_event="event_A"),
+        after=Campaign(id="doc4", status="active", trigger_event="event_C", exception_event="event_A"),
     ),
     CampaignChangeData(
-        op="d",
-        before=Campaign(id=2, trigger_event="event_B", exception_event=None),
-        after=None,
+        op="u",
+        before=Campaign(id="doc2", status="active", trigger_event="event_B", exception_event=None),
+        after=Campaign(id="doc2", status="stopped", trigger_event="event_B", exception_event=None),
     ),
+    CampaignChangeData(
+        op="u",
+        before=Campaign(id="doc3", status="active", trigger_event="event_A", exception_event="event_B"),
+        after=Campaign(id="doc3", status="stopped", trigger_event="event_A", exception_event="event_B"),
+    )
 ]
-campaign_change_stream = op.input("campaigns", flow, TestingSource(campaign_cdc_data))
-
-
-@dataclass
-class CampaignTriggerChangeRecord:
-    event: str
-    op: Literal["create", "delete"]
-    type: CampaignEventType
-    campaign_id: int
-
-
-def flat_campaign_change_datasets(item: CampaignChangeData) -> list[CampaignTriggerChangeRecord]:
-    out = []
-
-    def new_record(
-            _op: Literal["create", "delete"],
-            _type: CampaignEventType,
-            _data: Campaign,
-    ) -> CampaignTriggerChangeRecord:
-        return CampaignTriggerChangeRecord(
-            event=getattr(_data, _type),
-            op=_op,
-            type=_type,
-            campaign_id=_data.id,
-        )
-
-    match item.op:
-        case "c":
-            out.append(new_record("create", "trigger_event", item.after))
-            if item.after.exception_event:
-                out.append(new_record("create", "exception_event", item.after))
-        case "u":
-            if item.before.trigger_event != item.after.trigger_event:
-                out.append(new_record("delete", "trigger_event", item.before))
-                out.append(new_record("create", "trigger_event", item.after))
-            if item.before.exception_event != item.after.exception_event:
-                if item.before.exception_event:
-                    out.append(new_record("delete", "exception_event", item.before))
-                if item.after.exception_event:
-                    out.append(new_record("create", "exception_event", item.after))
-        case "d":
-            out.append(new_record("delete", "trigger_event", item.before))
-            if item.before.exception_event:
-                out.append(new_record("delete", "exception_event", item.before))
-
-    return out
-
-
-flatten_campaign_cdc = op.flat_map(
-    "flat_campaign_cdc",
-    campaign_change_stream,
-    flat_campaign_change_datasets,
-)
-# op.inspect("inspect_cdc", flatten_campaign_cdc)
-
-keyed_cdc = op.key_on("keyed_cdc", flatten_campaign_cdc, attrgetter("event"))
-# op.inspect("inspect_keyed_cdc", keyed_cdc)
-
-
-class EventState(dict[CampaignEventType, set[int]]):
-    def __init__(self, event: str):
-        super().__init__()
-        self.event = event
-        self["trigger_event"] = set[int]()
-        self["exception_event"] = set[int]()
-        self.in_use = self._check_in_use()
-
-    def apply(self, record: CampaignTriggerChangeRecord) -> None:
-        match record:
-            case CampaignTriggerChangeRecord(op="create", type=_type, campaign_id=campaign_id):
-                self[_type].add(campaign_id)
-            case CampaignTriggerChangeRecord(op="delete", type=_type, campaign_id=campaign_id):
-                self[_type].remove(campaign_id)
-
-        self.in_use = self._check_in_use()
-
-    def _check_in_use(self) -> bool:
-        return bool(self["trigger_event"]) or bool(self["exception_event"])
-
-
-def mapper(
-        state: EventState | None,
-        value: CampaignTriggerChangeRecord,
-) -> tuple[EventState | None, bool]:
-    if state is None:
-        state = EventState(value.event)
-    state.apply(value)
-    return state, state.in_use
-
-
-event_in_use_stream = op.stateful_map("stateful_triggers", keyed_cdc, mapper)
-# op.inspect("inspect_actions", actions)
-
-
-# [EVENT STREAM]
-# 1. Key by events
-# 2. Join event stream with event-action stream
-# 3. Key on user_id
-# 4. Stateful by user_id
-# 5. Sink to scheduler
-# 참고: 발송 완료 이벤트도 같이 들어온다. state 유지 시 사용
 events_data = [
     UserEventData(user_id=1, event_name="event_A"),
     UserEventData(user_id=1, event_name="event_A"),
@@ -164,17 +66,132 @@ events_data = [
     UserEventData(user_id=1, event_name="event_A"),
     UserEventData(user_id=1, event_name="event_A"),
 ]
-events = op.input("events", flow, TestingSource(events_data))
 
-# TODO: 추후에 running join이 아닌 enrich_cached 방식을 고려해보자.
-keyed_events = op.key_on("key_on_events", events, attrgetter("event_name"))
-keyed_joined_events = op.join("join_actions", keyed_events, event_in_use_stream, emit_mode="running")
-# op.inspect("inspect_keyed_joined_events", keyed_joined_events)
+campaign_change_stream = op.input("campaigns", flow, TestingSource(campaign_cdc_data))
+event_stream = op.input("events", flow, TestingSource(events_data))
 
 
-def as_campaign_service_request(item: tuple[UserEventData | None, bool | None]) -> UserEventMessage | None:
-    event, in_use = item
-    if (event is not None) and (in_use is True):
+# 1. change stream을 event로 분해한다. event마다 trigger/exclude 할 campaign-id 유지해야 함
+@dataclass
+class CampaignTriggerChangeRecord:
+    event_name: str
+    op: Literal["set", "unset"]
+    type: CampaignEventType
+    campaign_id: str
+
+
+def flat_campaign_change_datasets(item: CampaignChangeData) -> list[CampaignTriggerChangeRecord]:
+    out = []
+
+    def new_record(
+            _op: Literal["set", "unset"],
+            _type: CampaignEventType,
+            _data: Campaign,
+    ) -> CampaignTriggerChangeRecord:
+        return CampaignTriggerChangeRecord(
+            event_name=getattr(_data, _type),
+            op=_op,
+            type=_type,
+            campaign_id=_data.id,
+        )
+
+    match item.op:
+        case "c":
+            out.append(new_record("set", "trigger_event", item.after))
+            if item.after.exception_event:
+                out.append(new_record("set", "exception_event", item.after))
+
+        case "u":
+            if item.before.trigger_event != item.after.trigger_event:
+                out.append(new_record("unset", "trigger_event", item.before))
+                out.append(new_record("set", "trigger_event", item.after))
+            if item.before.exception_event != item.after.exception_event:
+                if item.before.exception_event:
+                    out.append(new_record("unset", "exception_event", item.before))
+                if item.after.exception_event:
+                    out.append(new_record("set", "exception_event", item.after))
+            if item.before.status != item.after.status:
+                if item.after.status == "active":
+                    out.append(new_record("set", "trigger_event", item.after))
+                    if item.after.exception_event:
+                        out.append(new_record("set", "exception_event", item.after))
+                elif item.before.status == "active":
+                    out.append(new_record("unset", "trigger_event", item.before))
+                    if item.before.exception_event:
+                        out.append(new_record("unset", "exception_event", item.before))
+
+        case "d":
+            out.append(new_record("unset", "trigger_event", item.before))
+            if item.before.exception_event:
+                out.append(new_record("unset", "exception_event", item.before))
+
+    return out
+
+
+campaign_cdc = op.flat_map(
+    "flat_campaign_cdc",
+    campaign_change_stream,
+    flat_campaign_change_datasets,
+)
+# op.inspect("inspect_cdc", campaign_cdc)
+
+keyed_campaign_cdc = op.key_on("keyed_cdc", campaign_cdc, attrgetter("event_name"))
+# op.inspect("inspect_keyed_campaign_cdc", keyed_campaign_cdc)
+
+keyed_event = op.key_on("keyed_event", event_stream, attrgetter("event_name"))
+# op.inspect("inspect_keyed_event", keyed_event)
+
+
+@dataclass
+class EventState:
+    event: str = field(init=True)
+    in_use: bool = field(default=False, init=False)
+
+    _trigger_campaigns: set[str] = field(default_factory=set, init=False)
+    _exception_campaigns: set[str] = field(default_factory=set, init=False)
+
+    def update(self, record: CampaignTriggerChangeRecord) -> None:
+        match record:
+            case CampaignTriggerChangeRecord(op="set", type="trigger_event", campaign_id=campaign_id):
+                self._trigger_campaigns.add(campaign_id)
+            case CampaignTriggerChangeRecord(op="set", type="exception_event", campaign_id=campaign_id):
+                self._exception_campaigns.add(campaign_id)
+            case CampaignTriggerChangeRecord(op="unset", type="trigger_event", campaign_id=campaign_id):
+                self._trigger_campaigns.discard(campaign_id)
+            case CampaignTriggerChangeRecord(op="unset", type="exception_event", campaign_id=campaign_id):
+                self._exception_campaigns.discard(campaign_id)
+
+        self.in_use = bool(self._trigger_campaigns) or bool(self._exception_campaigns)
+
+
+def mapper(
+        state: EventState | None,
+        value: CampaignTriggerChangeRecord | UserEventData,
+) -> tuple[EventState | None, UserEventData | None]:
+    if state is None:
+        state = EventState(value.event_name)
+
+    if isinstance(value, CampaignTriggerChangeRecord):
+        state.update(value)
+        value = None
+    elif isinstance(value, UserEventData):
+        if not state.in_use:
+            value = None
+    else:
+        value = None
+
+    return state, value
+
+
+merged_stream = op.merge("merged", keyed_campaign_cdc, keyed_event)
+# op.inspect("inspect_merged", merged_stream)
+
+eval_stream = op.stateful_map("state", merged_stream, mapper)
+# op.inspect("inspect_eval_stream", eval_stream)
+
+
+def as_campaign_service_request(event: UserEventData | None) -> UserEventMessage | None:
+    if event is not None:
         return UserEventMessage(
             user_id=event.user_id,
             event_data={
@@ -186,37 +203,10 @@ def as_campaign_service_request(item: tuple[UserEventData | None, bool | None]) 
         )
 
 
-keyed_requests = op.filter_map_value("filter_value", keyed_joined_events, as_campaign_service_request)
+keyed_requests = op.filter_map_value("filter_value", eval_stream, as_campaign_service_request)
 # op.inspect("inspect_keyed_requests", keyed_requests)
 
 requests = op.key_rm("unkey_requests", keyed_requests)
 # op.inspect("inspect_requests", requests)
 
 op.output("sink_to_executor", requests, sink=CampaignServiceSink())
-
-# def flat_requests(item: tuple[UserEvent | None, EventState | None]) -> list[CampaignTrigger]:
-#     # join 된 tuple 중 유효한 것만 살리고, 하나로 merge 한다.
-#     event, action = item
-#     if (event is None) or (action is None):
-#         return []
-#
-#     # TODO: campaign state가 변경된 경우에는 skip 할 수 있을까?
-#
-#     def create_requests(_type: CampaignEventType):
-#         return [
-#             CampaignTrigger(
-#                 type=_type,
-#                 campaign_id=campaign_id,
-#                 user_id=event.user_id,
-#             )
-#             for campaign_id in action[_type]
-#         ]
-#
-#     return create_requests("trigger_event") + create_requests("exception_event")
-#
-#
-# keyed_requests = op.flat_map_value("flat_requests", keyed_joined_events, flat_requests)
-# # op.inspect("inspect_keyed_requests", keyed_requests)
-# requests = op.key_rm("unkey_requests", keyed_requests)
-# # op.inspect("inspect_requests", requests)
-# op.output("sink_to_executor", requests, sink=CampaignServiceSink())
--- stream-processor/app/model.py
+++ stream-processor/app/model.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from typing import Any, Generic, Literal, TypeVar
 
 from pydantic import BaseModel
@@ -5,8 +6,15 @@ from pydantic import BaseModel
 DataT = TypeVar("DataT")
 
 
+class Filter(BaseModel):
+    name: str
+    operator: Literal["eq", "ne", "gt", "gte", "lt", "lte"]  # 일단
+    condition_value: Any
+
+
 class Campaign(BaseModel):
-    id: int
+    id: str
+    status: str
     trigger_event: str
     exception_event: str | None
 
@@ -24,11 +32,3 @@ class ChangeData(BaseModel, Generic[DataT]):
 
 
 CampaignChangeData = ChangeData[Campaign]
-
-CampaignEventType = Literal["trigger_event", "exception_event"]
-
-
-# class CampaignTrigger(BaseModel):
-#     type: CampaignEventType
-#     campaign_id: int
-#     user_id: int
